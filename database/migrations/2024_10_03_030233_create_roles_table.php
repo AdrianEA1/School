0930771406f51038723e1bb0eea2dd8ca6a7d627
Jonathan Rodriguez <21030021@itcelaya.edu.mx>
2024-10-02 21:21:07 -0600

    Corrigiendo el modelo roles

--- database/migrations/2024_10_03_030233_create_roles_table.php
+++ database/migrations/2024_10_03_030233_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $rol->char(30);
+            $table->char('rol', length: 30);
             $table->timestamps();
         });
     }
