e2e159b0c24909865c9c122e8398f57ef479e37b
Adrián Estrada Álvarez <21030975@itcelaya.edu.mx>
2024-10-03 20:07:16 -0600

    Subiendo el modelo Reports y corrigiendo algo de .env

---
+++ database/migrations/2024_10_04_014445_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->char('maestro', length: 50);
+            $table->char('tipo', length: 30);
+            $table->date('fecha');
+            $table->char('motivo', length: 150);
+            $table->boolean('confirmacion')->default(0);
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
