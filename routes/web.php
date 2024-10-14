d7d1b0dbc4b3b0db48b3345cba0e26e2398f0475
Erick <21030413@itcelaya.edu.mx>
2024-10-13 21:20:07 -0600

    Actualizacion web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', [SiteController::class, 'index'])->name('home');
+
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('auth.loginForm');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
